a04d5c7e88f73184258cd879e301da5913f6e2bb
Herbert Nordson <herbert_nordson@hotmail.com>
2022-01-10 20:37:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+class Client {
+  name;
+  cpf;
+  ag;
+  amount;
+}
+
+const client_1 = new Client();
+
+client_1.name = "Ricardo";
+client_1.cpf = 04410230530;
+client_1.ag = 1001;
+client_1.amount = 0;
+
+console.log(client_1);
