b99683be6a7737ece271ee9dc851a88a8a22e2e5
Herbert Nordson <herbert_nordson@hotmail.com>
2022-01-10 21:14:23 -0300

    Estudo da aplicação do JS com Orientacao a Objt

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 class Client {
   name;
   cpf;
+}
+
+class checkingAccount {
   ag;
-  amount;
+  _amount = 0;
+
+  withdraw = (value) => {
+    if (this._amount >= value) return (this._amount -= value);
+  };
+
+  deposit = (value) => {
+    if (value > 0) return (this._amount += value);
+  };
 }
 
 const client_1 = new Client();
+const client_account = new checkingAccount();
 
 client_1.name = "Ricardo";
-client_1.cpf = 04410230530;
-client_1.ag = 1001;
-client_1.amount = 0;
+client_1.cpf = 11122233344;
 
 console.log(client_1);
+
+client_account.ag = 1001;
+
+client_account.deposit(100);
+
+console.log(client_account._amount);
+
+client_account.withdraw(50);
+
+console.log(client_account._amount);
