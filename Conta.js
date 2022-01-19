1e65dd8ce5444e9dce000dc96ad4db3769f7a431
Herbert Nordson <herbert_nordson@hotmail.com>
2022-01-18 23:43:59 -0300

    feat: tornando o metodo _sacar dinamico pra reutil

--- Conta.js
+++ Conta.js
@@ -18,14 +18,20 @@ export class Conta {
   get saldo() {
     return this._saldo;
   }
+
   sacar(valor) {
     let taxa = 1;
+    return this._sacar(valor, taxa);
+  }
+
+  _sacar(valor, taxa) {
     const valorSacado = taxa * valor;
-    taxa = 1.1 * valor;
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
+    if (this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
     }
+
+    return 0;
   }
 
   depositar(valor) {
