b097b39b2f8614a280cf93de552c34c3369b30f4
Herbert Nordson <herbert_nordson@hotmail.com>
2022-01-18 22:41:30 -0300

    feat: alterações para reinício de projeto!

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import { CheckingAccount } from "./CheckingAccount.js";
-import { Client } from "./Client.js";
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
-const client_1 = new Client("Ricardo", 11122233344);
-const client_2 = new Client("Angel", 34522233344);
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const client_account = new CheckingAccount(1001, client_1);
-const client_account2 = new CheckingAccount(1001, client_2);
 
-client_account.deposit(100);
-client_account.withdraw(50);
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-console.log(client_account._client);
-console.log("Ag: ", client_account.ag);
-console.log("Balance: $", client_account.amount);
-client_account.transfer(20, client_account2);
-console.log("--------------------------------------");
-console.log(client_account2._client);
-console.log("Ag: ", client_account2.ag);
-console.log("Balance: $", client_account2.amount);
-console.log("--------------------------------------");
-console.log("Account active: ", CheckingAccount.numberAccounts);
+const conta2 = new ContaCorrente(102, cliente2);
+
+let valor = 200;
+contaCorrenteRicardo.tranferir(valor, conta2);
+
+console.log(contaCorrenteRicardo);
